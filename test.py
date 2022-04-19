85022940a271c1d4e446dd954d935e897d770b53
Elias <elias@holamicasa.com>
2022-04-18 23:06:10 -0400

    init verifier

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 from smtplib import SMTP
 
-with SMTP("bensemann.co.nz") as smtp:
-    smtp.noop()
+# with SMTP("bensemann.co.nz") as smtp:
+#     smtp.noop()
+
+def sendEhlo():
+    connection = SMTP("smtp.gmail.com", 587)
+    connection.ehlo()
+
+sendEhlo()
