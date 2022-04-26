474612ba782b58f4cbcee858f5b5040caac70930
Elias <elias@holamicasa.com>
2022-04-26 01:14:00 -0400

    update bulk-verifier

--- bulk-verifier.py
+++ bulk-verifier.py
@@ -1,24 +1,34 @@
 from verifier import Verifier
 import time
 import threading
+import csv
 
-TotalNumberOfThreads = 1
-
-EMAILS = [
-    "owen@perennialbuilders.com",
-    "office@unionsportssales.com"
+TotalNumberOfThreads = 3
+InputFiles = [
+    "./input/verifier/emails_1.csv",
+    "./input/verifier/emails_2.csv",
+    "./input/verifier/emails_3.csv",
 ]
-
 class myThread(threading.Thread):
-    def __init__(self, threadID, name, counter):
+    def __init__(self, threadID, name, inputFile, counter):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
+        self.inputFile = inputFile
         self.counter = counter
 
     def run(self):
         print("Starting " + self.name)
         self.main(self.name, self.counter)
+
+    def readCsv(self, _filename):
+        result = []
+        with open(_filename, newline='', encoding='utf-8') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',')
+            for row in spamreader:
+                result.append(row)
+
+        return result
 
     def verifying(self, _email, _threadName):
         try:
@@ -32,15 +42,17 @@ class myThread(threading.Thread):
 
     def main(self, _threadName, _delay):
         time.sleep(_delay)
-        for email in EMAILS:
-            print('[' + _threadName + ']')
+        contacts = self.readCsv(self.inputFile)
+        for contact in contacts:
+            email = contact[0]
+            # print('[' + _threadName + ']')
             self.verifying(email, _threadName)
         
 
 if __name__ == '__main__':
     threads = []
     for i in range(TotalNumberOfThreads):
-        threads.append(myThread(i, "EMAIL_VERIFYING_THREAD_" + str(i), 3 * i))
+        threads.append(myThread(i, "EMAIL_VERIFYING_THREAD_" + str(i), InputFiles[i], 3 * i))
 
     for thread in threads:
         thread.start()
--- verifier.py
+++ verifier.py
@@ -1,6 +1,9 @@
 import dns.resolver
 import csv
+import time
+import os
 
+ts = str(time.time())
 class Verifier:
     def __init__(self):
         self.domain = 'https://www.google.com'
@@ -15,7 +18,11 @@ class Verifier:
         return result
 
     def writeCsv(self, _filename, _row):
-        with open(_filename, 'a', newline='', encoding='utf-8') as f:
+        directory = './results/verifier'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        with open(directory + '/' + _filename, 'a', newline='', encoding='utf-8') as f:
             writer = csv.writer(f, delimiter =',')
             writer.writerow(_row)
 
@@ -29,7 +36,18 @@ class Verifier:
 
         return mxRecord
 
-    def isOffice365(self, _mxRecord):
+    def spfLookup(self, _domain):
+        spfRecord = ''
+        try:
+            for x in dns.resolver.resolve(_domain, 'TXT'):
+                if 'spf1' in str(x):
+                    spfRecord = str(x).split(' ')[1].split(':')[1]
+        except:
+            spfRecord = 'nospfrecord.com.'
+        
+        return spfRecord
+
+    def isOffice365FromMX(self, _mxRecord):
         try:
             domainId = _mxRecord.split('.')[::-1][2]
             if domainId == 'outlook':
@@ -39,12 +57,10 @@ class Verifier:
         except:
             return False
 
-    def isGodaddy(self, _mxRecord):
+    def isOffice365FromSpf(self, _spfRecord):
         try:
-            domainId = _mxRecord.split('.')[::-1][2]
-            domainAppendix = _mxRecord.split('.')[::-1][1]
-            
-            if domainId == 'secureserver' and domainAppendix == 'net':
+            domainId = _spfRecord.split('.')[::-1][1]
+            if domainId == 'outlook':
                 return True
             else:
                 return False
@@ -64,17 +80,18 @@ class Verifier:
                 self.writeCsv('initial-3.unique.csv', [email])
 
     def startVerifying(self, _email, _threadName):
-        # self.writeCsv('/verifier-result/result-office365.csv', ['email', 'domain', 'mxRecord'])
-
         try:
             domain = _email.split('@')[1].lower()
-            mxRecord = self.mxLookup(domain)
+            # mxRecord = self.mxLookup(domain)
+            spfRecord = self.spfLookup(domain)
+            # print('[mxRecord]', mxRecord)
+            # print('[spfRecord]', spfRecord)
             
-            if self.isOffice365(mxRecord):
-                # self.writeCsv('/verifier-result/result-office365.csv', [_email, domain, mxRecord])
-                print('[TRUE]', _email, domain, mxRecord)
+            if self.isOffice365FromSpf(spfRecord):
+                self.writeCsv('result-office365-' + ts + '.csv', [_email])
+                print('[TRUE]', _email, domain, spfRecord)
             else:
-                print('[FALSE]', _email, domain, mxRecord)
+                print('[FALSE]', _email, domain, spfRecord)
         except Exception as ex:
             template = "An exception of type {0} occurred. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
